5eecfb521c17d13fcb3099fecf0420560fda58a3
Perry <perrylu21@gmail.com>
2023-02-02 22:31:00 +0800

    update app.py

--- app.py
+++ app.py
@@ -15,8 +15,19 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
+@app.route("/params",methods=["GET"])
+def get_params():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    text_msg ="My name is " + name + " and I am " + str(age) + " years old" 
+    try:
+        line_bot_api.push_message(user_id,TextSendMessage(text=text_msg))
+    except LineBotApiError as e:
+        print("LineBot Error:{0}".format(e.message))
+    return jsonify(message=text_msg)
 
-@app.route("/", methods=["GET", "POST"])
+#@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["POST"])
 def callback():
 
     if request.method == "GET":
